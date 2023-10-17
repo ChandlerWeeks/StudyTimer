205fdbc5244025368e622c4501e239d70228ab2a
Jason Weeks <49565288+ChandlerWeeks@users.noreply.github.com>
2023-10-17 16:42:28 -0500

    InitializeProject
    Added All files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # StudyTimer
- This is a simple HTML, CSS, and JS based timer for study intervals of 25 minutes. This is Day1 for the 100 days of code challenge.
+ This is a simple HTML, CSS, and JS based timer for study intervals of 25 minutes. This is Day1 for the 100 days of code challenge. 
+
+ My goal with this project is to refresh my ability to use the fundamentals of web development, javascript, HTML, and CSS
---
+++ timer.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // elements 
+    const startButton = document.querySelector('#start-button')
+    const pauseButton = document.querySelector('#pause-button')
+    const resetButton = document.querySelector('#reset-button')
+    const timer = document.querySelector('#time-string')
+
+    // variables
+    const defaultMins = 25
+    const defaultSecs = 0;
+    let mins = defaultMins;
+    let secs = defaultSecs;
+    let timerString = `${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+    let running = false
+
+
+    resetButton.addEventListener('click', () => {
+    if (running) {
+        clearInterval(intervalID);
+        running = false
+    }
+    mins = defaultMins;
+    secs = defaultSecs;
+    timerString = `${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+    timer.innerText = timerString;
+    });
+
+    let intervalID = null
+    startButton.addEventListener('click', () => {
+    if (!running) {
+        running = true
+        intervalID = setInterval(decrementTime, 1000)
+    }
+    });
+
+    pauseButton.addEventListener('click', () => {
+    if (running) {
+        clearInterval(intervalID);
+        running = false
+    }
+    });
+
+    function decrementTime() {
+    if (secs < 1) {
+        if (mins < 1) {
+        alert('Alarm Done! Take a 5 Minute break')
+        } else {
+        mins -= 1
+        secs = 59
+        }
+    } else {
+        secs -= 1
+    }
+    const timerString = `${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+    timer.innerText = timerString;
+    }
+});
